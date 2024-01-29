6b0356ef9023b4e0a98a2e298b1f8f53921590ed
Johny270 <90469202+Johny270@users.noreply.github.com>
2024-01-29 10:59:20 -0400

    Add validation page to user input

---
+++ PartyInvites/Models/Repository.cs
@@ -0,0 +1,15 @@
+﻿namespace PartyInvites.Models
+{
+    public static class Repository
+    {
+        public static List<GuestResponse> responses = new();
+
+        public static IEnumerable<GuestResponse> Responses => responses;
+
+        public static void AddResponse(GuestResponse response)
+        {
+            Console.WriteLine(response);
+            responses.Add(response);
+        }
+    }
+}
