574ecf0b46a57ac073bc3c51134f74d2d3c8fe30
Johny270 <90469202+Johny270@users.noreply.github.com>
2024-01-29 09:23:48 -0400

    Initial commit

---
+++ FirstProject/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿//using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+//using FirstProject.Models;
+
+namespace FirstProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            string viewModel = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("MyView", viewModel);
+        }
+
+    }
+}
+
+
