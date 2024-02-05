6857fc9148841d65b09f8b990a95ad9a95797ddc
Johny270 <90469202+Johny270@users.noreply.github.com>
2024-02-04 21:45:10 -0400

    Configure Entity Framework Core

--- SportsSln/SportsStore/Models/StoreDbContext.cs
+++ SportsSln/SportsStore/Models/StoreDbContext.cs
@@ -2,6 +2,7 @@
 
 namespace SportsStore.Models
 {
+    // Entity Framework provides access to the database through a context class
     public class StoreDbContext : DbContext
     {
         public StoreDbContext(DbContextOptions<StoreDbContext> options) : base(options) { }
--- SportsSln/SportsStore/Program.cs
+++ SportsSln/SportsStore/Program.cs
@@ -1,6 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+
+
+// The following block of code describe the type of database to which Entity Framework Core will connect,
+// it specify the connection string that describes that connection, and also specify the content class that
+// will present the data in the database.
+
+builder.Services.AddDbContext<StoreDbContext>(opts =>
+{
+    opts.UseSqlServer(builder.Configuration["ConnectionStrings:SportsStoreConnection"]);
+});
 
 var app = builder.Build();
 
