12d333505357d439a23422f3b825b3e33188ac6c
Johny270 <90469202+Johny270@users.noreply.github.com>
2024-02-05 00:22:50 -0400

    Implement pagination functionality and write unit test to test the functionalityu

--- SportsSln/SportsStore/Controllers/HomeController.cs
+++ SportsSln/SportsStore/Controllers/HomeController.cs
@@ -6,11 +6,13 @@ namespace SportsStore.Controllers
     public class HomeController: Controller
     {
         private IStoreRepository repository;
+        public int PageSize = 4;
 
         public HomeController(IStoreRepository repo)
         {
             repository = repo;
         }
-        public IActionResult Index() => View(repository.Products);
+        public IActionResult Index(int productPage = 1) => 
+            View(repository.Products.OrderBy(p => p.ProductID).Skip((productPage - 1) * PageSize).Take(PageSize));
     }
 }
