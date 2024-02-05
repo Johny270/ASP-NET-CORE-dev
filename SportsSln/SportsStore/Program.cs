5634d2c8713056017a43c3322f1b19d8ee3aa3c9
Johny270 <90469202+Johny270@users.noreply.github.com>
2024-02-04 22:01:42 -0400

    Create a repository and a class implementation for that repository

--- SportsSln/SportsStore/Program.cs
+++ SportsSln/SportsStore/Program.cs
@@ -5,19 +5,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
-
-// The following block of code describe the type of database to which Entity Framework Core will connect,
-// it specify the connection string that describes that connection, and also specify the content class that
-// will present the data in the database.
-
+/*
+ * builder.Configuration provides access to the configuration data, thus allowing the database connection
+ * string to be retrieved. 
+ * AddDbContext configures Entity Framework Core, registers the database context class and configures the 
+ * relationship with the database.
+ * 
+*/
 builder.Services.AddDbContext<StoreDbContext>(opts =>
 {
     opts.UseSqlServer(builder.Configuration["ConnectionStrings:SportsStoreConnection"]);
 });
 
 var app = builder.Build();
-
-//app.MapGet("/", () => "Hello World!");
 
 app.UseStaticFiles();
 app.MapDefaultControllerRoute();
