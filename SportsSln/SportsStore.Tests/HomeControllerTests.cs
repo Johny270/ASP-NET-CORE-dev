dd29ac6562798c0e7ad42a099a26a30ab2bc0225
Johny270 <90469202+Johny270@users.noreply.github.com>
2024-02-07 21:12:20 -0400

    Test the filter products by category feature of the Index action method in HomeController

--- SportsSln/SportsStore.Tests/HomeControllerTests.cs
+++ SportsSln/SportsStore.Tests/HomeControllerTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -98,6 +99,35 @@ namespace SportsStore.Tests
             Assert.Equal(3, pageInfo.ItemsPerPage);
             Assert.Equal(5, pageInfo.TotalItems);
             Assert.Equal(2, pageInfo.TotalPages);
+        }
+
+        [Fact]
+        public void Can_Filter_Product()
+        {
+            // Arrange
+            // - Create the mock repository
+            Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1", Category = "Cat1"},
+                new Product {ProductID = 2, Name = "P2", Category = "Cat2"},
+                new Product {ProductID = 3, Name = "P3", Category = "Cat1"},
+                new Product {ProductID = 4, Name = "P4", Category = "Cat2"},
+                new Product {ProductID = 5, Name = "P5", Category = "Cat1"},
+            }).AsQueryable<Product>());
+
+            // Arrange - Create a controller and make the page size 3 items
+            HomeController controller = new HomeController(mock.Object);
+            controller.PageSize = 3;
+
+            // Action
+            Product[] result = (controller.Index("Cat2", 1)?.ViewData.Model as ProductsListViewModel ?? new())
+                .Products.ToArray();
+
+            // Assert
+            Assert.Equal(2, result.Length);
+            Assert.True(result[0].Name == "P2" && result[0].Category == "Cat2");
+            Assert.True(result[1].Name == "P4" && result[1].Category == "Cat2");
         }
     }
 }
