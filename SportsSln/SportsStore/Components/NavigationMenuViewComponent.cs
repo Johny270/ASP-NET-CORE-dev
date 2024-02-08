3758970522a4d40715a0d80d752b85833ea56a3a
Johny270 <90469202+Johny270@users.noreply.github.com>
2024-02-07 21:58:04 -0400

    Create a view component for category Navigation component

---
+++ SportsSln/SportsStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        private IStoreRepository repository;
+
+        public NavigationMenuViewComponent(IStoreRepository repo)
+        {
+            repository = repo;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(repository.Products.Select(x => x.Category).Distinct().OrderBy(x => x));
+        }
+    }
+}
